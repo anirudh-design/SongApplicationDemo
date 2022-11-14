c101e8c50fc4b7948b31e9aae5b746bca44d9cfe
anisingh3012 <anisingh30121998@gmail.com>
2022-11-14 22:50:29 +0530

    Second commit

--- src/Album.java
+++ src/Album.java
@@ -1,3 +1,31 @@
-public class Album {
+import java.util.ArrayList;
 
+public class Album {
+    private String name;
+    private String artist;
+    private ArrayList<Song> songs;
+
+    public Album(String name, String artist) {
+        this.name = name;
+        this.artist = artist;
+        this.songs = new ArrayList<>();
+    }
+
+    public boolean findSong(String title){
+        for(Song checkSong:this.songs){
+            if(checkSong.getTitle().equals(title)) {
+                System.out.println("Song Found !!");
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean addSong(String title, double duration){
+        if(findSong(title)==true){
+            System.out.println("Song already present");
+            return false;
+        }
+        return this.songs.add(new Song(title, duration));
+    }
 }
